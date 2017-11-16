2fc14575150431247e7b0f61a4c2737890355b76
方朝龙 <2925725541@qq.com>
2017-11-16 21:06:08 +0800

    阿阿强

---
+++ vlivli/src/components/demo/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div>
+			denglu
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		components:{
+			
+		},
+		
+		
+		beforeMount(){
+			this.$emit("to-parent",false);
+		}
+	}
+</script>
+
+<style>
+</style>
